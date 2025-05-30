058e5783567b04720a0cbac1ebb168a89ab616c8
z <>
2025-05-30 23:57:45 +0800

    code from v2.2.0 to fix jpa bug(wont clear jpa cache) which began from v2.2.7

--- app/src/main/java/com/fongmi/android/tv/player/extractor/JianPian.java
+++ app/src/main/java/com/fongmi/android/tv/player/extractor/JianPian.java
@@ -3,51 +3,46 @@ package com.fongmi.android.tv.player.extractor;
 import android.net.Uri;
 
 import com.fongmi.android.tv.player.Source;
-import com.github.catvod.utils.Path;
 import com.p2p.P2PClass;
 
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JianPian implements Source.Extractor {
 
     private P2PClass p2p;
     private String path;
-    private Map<String, Boolean> pathPaused;
 
     @Override
     public boolean match(String scheme, String host) {
-        return "tvbox-xg".equals(scheme) || "jianpian".equals(scheme) || "ftp".equals(scheme);
+        return scheme.equals("tvbox-xg") || scheme.equals("jianpian") || scheme.equals("ftp");
     }
 
     private void init() {
         if (p2p == null) p2p = new P2PClass();
-        if (pathPaused == null) pathPaused = new HashMap<>();
     }
 
     @Override
     public String fetch(String url) throws Exception {
         init();
         stop();
-        start(url);
+        set(url);
+        start();
         return "http://127.0.0.1:" + p2p.port + "/" + URLEncoder.encode(Uri.parse(path).getLastPathSegment(), "GBK");
     }
 
-    private void start(String url) {
+    private void set(String url) {
+        path = URLDecoder.decode(url).split("\\|")[0];
+        path = path.replace("jianpian://pathtype=url&path=", "");
+        path = path.replace("tvbox-xg://", "").replace("tvbox-xg:", "");
+        path = path.replace("xg://", "ftp://").replace("xgplay://", "ftp://");
+    }
+
+    private void start() {
         try {
-            String lastPath = path;
-            path = URLDecoder.decode(url).split("\\|")[0];
-            path = path.replace("jianpian://pathtype=url&path=", "");
-            path = path.replace("tvbox-xg://", "").replace("tvbox-xg:", "");
-            path = path.replace("xg://", "ftp://").replace("xgplay://", "ftp://");
-            boolean isDiff = lastPath != null && !lastPath.equals(path);
-            if (isDiff) p2p.P2Pdoxdel(lastPath.getBytes("GBK"));
+            if (p2p == null || path == null) return;
             p2p.P2Pdoxstart(path.getBytes("GBK"));
-            if (lastPath == null || isDiff) p2p.P2Pdoxadd(path.getBytes("GBK"));
-            if (isDiff && pathPaused.containsKey(lastPath)) pathPaused.remove(lastPath);
-            pathPaused.put(path, false);
+            p2p.P2Pdoxadd(path.getBytes("GBK"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,9 +52,9 @@ public class JianPian implements Source.Extractor {
     public void stop() {
         try {
             if (p2p == null || path == null) return;
-            if (pathPaused.containsKey(path) && pathPaused.get(path)) return;
             p2p.P2Pdoxpause(path.getBytes("GBK"));
-            pathPaused.put(path, true);
+            p2p.P2Pdoxdel(path.getBytes("GBK"));
+            path = null;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -67,6 +62,5 @@ public class JianPian implements Source.Extractor {
 
     @Override
     public void exit() {
-        Path.clear(Path.jpa());
     }
 }
